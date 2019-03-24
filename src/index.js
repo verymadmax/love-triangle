15273f6a3bacd85a0e6e235ca6fd3135f5878f81
verymadmax <madmax.verymad@gmail.com>
2019-03-24 18:25:31 +0300

    done

--- src/index.js
+++ src/index.js
@@ -3,5 +3,19 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var total = 0;  
+  var countTriangles = 0;
+     for ( var i = 0; i < preferences.length; i++){ 
+      var first = preferences [i];
+      var second = preferences [first - 1];
+      var third = preferences [second - 1];
+      if (third === i + 1){
+        countTriangles++;
+      }
+      if (countTriangles == 3){
+        total++;
+        countTriangles = 0;
+      }
+     }
+    return total;
 };
